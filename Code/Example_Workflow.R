c24dabc8762376513226debd4c56f515b911012c
Aniko B Toth <abtoth2@gmail.com>
2021-09-08 17:46:15 +1000

    write example workflow for spatially processing multiple ecosystems

--- Code/Example_Workflow.R
+++ Code/Example_Workflow.R
@@ -4,38 +4,56 @@ library(parallel)
 ## load functions
 source("./Code/Helper_functions.R")
 
-## spatial processing 
-
-ecosystem <- st_read("../Spatialdata_forAniko/TECDist/TEC5", "NSWTEC5_ShaleSandSydney") 
-fire <- st_read("../Spatialdata_forAniko/FireNPWSHistory/firenpwsfirehistory", "Fire_NPWS_FireHistory") 
+# Load Fire data and assessment year
 assess_year <- c(2020, 2015, 2010, 2005, 2000)
-
 ncores <- min(length(assess_year), detectCores()) # ensure user's system is not overwhelmed
+
+## spatial processing for SINGLE ECOSYSTEM #####
+ecosystem <- st_read("../Spatialdata_forAniko/TECDist/TEC5", "NSWTEC5_ShaleSandSydney") 
 
 # parallelise to make it faster
 cl <- makeCluster(ncores)
 clusterEvalQ(cl,c(source('./Code/Helper_Functions.R')))
-clusterExport(cl, c("fire", "ecosystem"))
-
-# This took about 10 minutes on my computer
-FireHist <- parLapply(cl, assess_year, function(x) firehistory(ecosystem, fire, x)) 
+clusterExport(cl, c("ecosystem"))
+  
+ # This took about 10 minutes on my computer for TEC5
+FireHist <- parLapply(cl, assess_year, function(x) firehistory(ecosystem, x)) 
 FireHist <- bind_rows(FireHist)
+  
+## Spatial processing for MULTIPLE ECOSYSTEMS ############
+# for multiple ecosystems use a loop
 
-# To run subsets or multiple ecosystems, load new ecosystem shapefile and repeat. E.g.:
-    # ecosystem_subset <- st_read("path/name/here", "layer_name_here") 
-    # FireHist_subset <- parLapply(cl, assess_year, function(x) firehistory(ecosystem_subset, fire, x)) 
-    # FireHist_subset <- bind_rows(FireHist_subset)
+ecosystems <- list.files("../Spatialdata_forAniko/Spatialdata2_forAniko/", ".shp$") %>% 
+  word(1, 1, "\\.") %>% 
+  purrr::map(~st_read("../Spatialdata_forAniko/Spatialdata2_forAniko", .x))
 
-# Join into one table with ecosystems or subsets as columns
-    # FireHist <- full_join(FireHist, FireHist_subset, by = c("LastFire", "Assessment_Year"), suffix = c("_Full", "_Subset")) 
+FireHist <- list()
+for(i in seq_along(ecosystems)){
+  ecosystem <- ecosystems[[i]]
+  # parallelise to make it faster
+  cl <- makeCluster(ncores)
+  clusterEvalQ(cl,c(source('./Code/Helper_Functions.R')))
+  clusterExport(cl, c("ecosystem"))
+  
+  # This took about 10 minutes on my computer
+  FireHist[[i]] <- parLapply(cl, assess_year, function(x) firehistory(ecosystem, x)) 
+  FireHist[[i]] <- bind_rows(FireHist[[i]])
+  
+}
 
-# for a large number of ecosystems, could write a loop or helper function
+FireHist <- FireHist %>% 
+  setNames(list.files("../Spatialdata_forAniko/Spatialdata2_forAniko/", ".shp$") %>% 
+             word(1, 1, "\\.") %>% word(2,2, sep = "_")) %>% 
+  bind_rows(.id = "ecosystem") %>%  
+  pivot_wider(names_from = "ecosystem", values_from = "ObsAreaKm2", names_prefix = "ObsAreaKm2_", values_fill = 0)
+
 
 ## save as .csv to use with Shiny app
 write_csv(FireHist, "./Input_data.csv")
 
 # **** Input data MUST have columns "LastFire" and "Assessment_Year" as output by this code. *****
 # **** Ecosystem area columns MUST contain "ObsArea" (can be prefix or suffix, doesn't matter) ****
+# **** Separate "ObsArea" prefix or suffix from other identifiers using _ symbol ****
 
 
 ### Metric calculations #####
@@ -55,7 +73,7 @@ summaryTable <- outTable %>%
   group_by(Year, TSLF) %>% summarise(across(where(is.double), sum))
 
 # Plot frequency curves
-freq_curves(dat, yr = 2015)
+freq_curves(dat, yr = 2020)
 
 # Plot area graph 
 area_graph(shf)
